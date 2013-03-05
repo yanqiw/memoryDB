fabd525f11915760d68cd22baf97d39f3c8054b5
Frank(Wang.Yan.Qi) <wangyanq@gmail.com>
2013-03-06 00:49:06 +0800

    0.0.2
    Add a memoryDB level
    resturcture the code.

--- memoryDB.js
+++ memoryDB.js
@@ -1,114 +1,127 @@
-
-/*==================
- //Construction
- // paramter:
- // records: the data for init, array
- //=================*/
-function MDBtable(records){
-    this.table = {};
-    this.total = 0; // the total record number
-    this.counter = 0; // the un-deleted record number
-    if (records) {
-        for (; this.counter < records.length;) {
-            this.insert(records[this.counter]);
-        }
-    }
-}
-
-
-// insert record to the table
-MDBtable.prototype.insert = function(record){
-    record._id = generateUUID();
-    record._del = false;
-    this.table[record._id] = record
-    this.total++;
-    this.counter++;
-    return this.counter;
-}
-
-
-// update the record in table
-MDBtable.prototype.updateById = function(id,u){
-	for(key in u){
-		this.table[id][key]=u[key];
+var memorydb = (function() {
+	var mdb = {};
+	var _tables = {};
+	
+	mdb.newTable = function(tableName) {
+		if (_tables[tableName]) {
+			return false;
+		} else {
+			_tables[tableName] = MDBtable.createNew();
+			mdb[tableName] = _tables[tableName];
+		}
 	}
-
-}
-
-MDBtable.prototype.getById = function(id){
-    if (this.table[id]) {
-        return this.table[id];
-    }
-    else {
-        return null;
-    }
-}
-/*============================
- // query records in table
- // support operator: equal
- =============================*/
-MDBtable.prototype.query = function(query){
-    var result = [];
-    
-    for (var i in this.table) {
-        var matchflag = true;
-        //equal
-        for (var key in query) {
-            if (this.table[i]._del || this.table[i][key] != query[key]) {
-                matchflag = false;
-                break;
-            };
-                    }
-        // equal end			
-        if (matchflag) 
-            result.push(this.table[i]);
-    }
-    
-    
-    return result;
-    
-}
-
-//remove records in table
-MDBtable.prototype.removeById = function(id){
-
-    if (this.table[id]) {
-        this.table[id]._del = true;
-        this.counter--;
-        return this.counter;
-    }
-    
-    return false;
-    
-}
-
-// index the table
-
-MDBtable.prototype.index = function(){
-
-}
-
-// table to JSON
-MDBtable.prototype.toJSON = function(){
 	
-}
-
-// All the record in table to JSON
-MDBtable.prototype.toJSONAll = function(){
 	
-}
+	
+	var MDBtable = {
+		createNew : function() {
+			var _table = {};
+			var _total = 0;
+			var _counter = 0;
+			var mdbtable = {};
+			
+			mdbtable.getAll = function(){
+				var result = [];
+				for(var i in _table){
+					result.push(_table[i]);
+				}
+				return result;
+			}
+			
+			mdbtable.getCounter = function(){
+				return _counter;
+			}
+			
+			// insert record to the table
+			mdbtable.insert = function(record) {
+				record._id = _generateUUID();
+				record._del = false;
+				_table[record._id] = record
+				_total++;
+				_counter++;
+				return _counter;
+			}
+			// update the record in table
+			mdbtable.updateById = function(id, u) {
+				for (key in u) {
+					_table[id][key] = u[key];
+				}
 
-/*==================
- //tookit
- //
- //
- //=================*/
-function generateUUID(){
-    var d = new Date().getTime();
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c){
-        var r = (d + Math.random() * 16) % 16 | 0;
-        d = Math.floor(d / 16);
-        return (c == 'x' ? r : (r & 0x7 | 0x8)).toString(16);
-    });
-    return uuid;
-};
+			}
+
+			mdbtable.getById = function(id) {
+				if (_table[id]) {
+					return _table[id];
+				} else {
+					return null;
+				}
+			}
+			/*============================
+			 // query records in table
+			 // support operator: equal
+			 =============================*/
+			mdbtable.query = function(query) {
+				var result = [];
+
+				for (var i in _table) {
+					var matchflag = true;
+					//equal
+					for (var key in query) {
+						if (_table[i]._del || _table[i][key] != query[key]) {
+							matchflag = false;
+							break;
+						};
+					}
+					// equal end
+					if (matchflag)
+						result.push(_table[i]);
+				}
+
+				return result;
+
+			}
+			//remove records in table
+			mdbtable.removeById = function(id) {
+
+				if (_table[id]) {
+					_table[id]._del = true;
+					_counter--;
+					return _counter;
+				}
+
+				return false;
+
+			}
+			// index the table
+
+			mdbtable.index = function() {
+
+			}
+			// table to JSON
+			mdbtable.toJSON = function() {
+
+			}
+			// All the record in table to JSON
+			mdbtable.toJSONAll = function() {
+
+			}
+			return mdbtable;
+		}
+	};
+	
+	
+	// tookit
+	_generateUUID = function() {
+		var d = new Date().getTime();
+		var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+			var r = (d + Math.random() * 16) % 16 | 0;
+			d = Math.floor(d / 16);
+			return (c == 'x' ? r : (r & 0x7 | 0x8)).toString(16);
+		});
+		return uuid;
+	}
+	
+	//return the main object
+	return mdb;
+}
+)();
